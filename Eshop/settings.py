c45bb6374265e52b789f66c7f0878c2bf17a4d5b
sodiqdevpython <sodiqdevpython@gmail.com>
2023-11-21 05:11:31 +0500

    added allowed hosts

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-95t%=#4o3@l-(-%ok9*h%n3!0(sdchjn%+_$5#umaj-!3bg*7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.pythonanywhere.com']
 
 
 # Application definition
