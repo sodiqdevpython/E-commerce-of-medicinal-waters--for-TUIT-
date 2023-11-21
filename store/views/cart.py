35c21878cdeb5206005fba9e09f1fbd9d8f5354a
sodiqdevpython <sodiqdevpython@gmail.com>
2023-11-21 05:00:52 +0500

    responsive solution

--- store/views/cart.py
+++ store/views/cart.py
@@ -5,8 +5,6 @@ from store.models.customer import Customer
 from django.views import  View
 from store.models.product import Products
 
-from django.contrib.gis.geoip2 import GeoIP2
-
 class Cart(View):
     def get(self , request):
         ids = list(request.session.get('cart').keys())
